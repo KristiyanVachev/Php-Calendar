663415a0bc9ba97e5f197e0b06407801b314bc20
Kristiyan Vachev <typhon04@gmail.com>
2018-07-02 10:15:57 +0300

    Formated weekly calendar

--- public/weekly.php
+++ public/weekly.php
@@ -23,53 +23,61 @@ try {
 <a href="index.php">Back to home</a>
 
         <?php
-        for ($day = 1; $day <= 7; $day++) {
+        for ($day = 1; $day <= 5; $day++) {
 
-            echo "Monday";
+            echo "<div class=\"day-container\">";
 
-            echo "<div class=\"day\">";
-
-                for ($hour = 9; $hour <= 19; $hour++) {
-                    echo "<div class=\"day-hour\">$hour:00</div>";
+                switch ($day){
+                    case 1:
+                        echo "<h3>Monday</h3>";
+                    break;
+                    case 2:
+                        echo "<h3>Tuesday</h3>";
+                    break;
+                    case 3:
+                        echo "<h3>Wednesday</h3>";
+                    break;
+                    case 4:
+                        echo "<h3>Thursday</h3>";
+                    break;
+                    case 5:
+                        echo "<h3>Friday</h3>";
+                    break;
                 }
 
-            echo "</div>";
+                echo "<div class=\"day\">";
 
-            echo "<div class=\"day\">";
-
-                for ($hour = 9; $hour <= 19; $hour++) {
-                    echo "<div class=\"hour\">";
-                    $noLectures = true;
-
-                    foreach ($result as $lecture) :
-                                if ($lecture["startHour"] == $hour && $lecture["dayId"] == $day){
-                                    $noLectures = false;
-                                    $span = $lecture["endHour"] - $lecture["startHour"];
-                                    echo "<div class=\"event\" data-span=",$span ,">",$lecture["subjectName"] , "</div>";
-                                }
-                            endforeach;
-                    if ($noLectures){
+                    for ($hour = 9; $hour <= 19; $hour++) {
+                        echo "<div class=\"day-hour\">$hour:00</div>";
                     }
-                    echo "</div>";
-                }
 
                 echo "</div>";
+
+                echo "<div class=\"day\">";
+
+                $noLectures = true;
+
+                    for ($hour = 9; $hour <= 19; $hour++) {
+                        echo "<div class=\"hour\">";
+
+                            foreach ($result as $lecture) :
+                                    if ($lecture["startHour"] == $hour && $lecture["dayId"] == $day){
+                                        $noLectures = false;
+
+                                        echo "<div class=\"event event-start event-end\" data-span=",$lecture["duration"], ">", $lecture["subjectName"], " in room ", $lecture["semesterRoom"], "</div>";
+                                    }
+                            endforeach;
+
+                        if ($noLectures && $hour == 19){
+                            echo "<div > No lectures today</div>";
+                        }
+                        echo "</div>";
+                    }
+
+                    echo "</div>";
+
+            echo "</div>";
         }?>
 
 
 <?php require "templates/footer.php"; ?>
-
-<table border="1px">
-    <tr>
-        <td></td>
-        <td colspan="2"></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-</table>
